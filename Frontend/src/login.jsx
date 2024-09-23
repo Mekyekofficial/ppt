1349aa39bc011ae83564faed2bfeeb2943e7cffe
Suddhajit2003 <suddhajit2@gmail.com>
2024-09-24 02:02:13 +0530

    g

---
+++ Frontend/src/login.jsx
@@ -0,0 +1,17 @@
+<div className="login-container">
+        <h1>Log In</h1>
+        <form>
+          <div className="input-group">
+            <label htmlFor="email">Email or Phone</label>
+            <input type="text" id="email" name="email" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password (8-characters)</label>
+            <input type="password" id="password" name="password" />
+          </div>
+          <button type="submit">CONTINUE</button>
+          <button type="button">Continue with Google</button>
+        </form>
+        <a href="/forgot-password">Forgot Password?</a>
+        <p>Have a Q? <a href="/join-us">Join us!</a></p>
+      </div>
