3bdd43f76dbbffa3ddec46cee54bad72d0851fb1
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2024-09-26 03:32:14 +0530

    Refactor feed section component styles and structure

---
+++ Frontend/src/components/feedSection.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+// import Profile from './Feed/profile';
+// import Feeds from './Feed/feeds';
+// import UserSuggestions from './Feed/userSuggestions';
+
+function feedSection() {
+  return (
+    <div className="feed-section">
+      <div className="left-sidebar">
+        {/* <Profile /> */}
+      </div>
+      <div className="main-content">
+        {/* <Feeds /> */}
+      </div>
+      <div className="right-sidebar">
+        {/* <UserSuggestions /> */}
+      </div>
+    </div>
+  );
+}
+
+export default feedSection;
