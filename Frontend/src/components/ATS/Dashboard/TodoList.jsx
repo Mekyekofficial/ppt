6d81291605667443c3e0b2582a7fcc4798bf5c5b
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2024-10-01 01:59:32 +0530

    Refactor TodoList component to update styling and CSS file

--- Frontend/src/components/ATS/Dashboard/TodoList.jsx
+++ Frontend/src/components/ATS/Dashboard/TodoList.jsx
@@ -4,7 +4,7 @@ import './css/TodoList.css';
 const TodoList = () => {
   return (
     <div className="todo-list">
-      <button className="add-todo-btn">+ Add ToDo List</button>
+      <button className="add-todo-btn"><span>+</span><br /> Add <br />ToDo List</button>
     </div>
   );
 };
