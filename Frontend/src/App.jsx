3fb57a60f6c69c85acc600b3da996cf913b7c9b4
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2024-09-30 23:43:43 +0530

    Refactor CSS to add custom scrollbar styling

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,19 +3,16 @@ import "./App.css";
 import Header from "./components/includes/header";
 import Footer from "./components/includes/footer";
 import FeedSection from "./components/feedSection";
-import LoginPopup from "./components/LoginPopup";
-import SignupPage from "./components/singup";
-import NameModal from "./components/nameModal";
-import BirthModal from "./components/birthModal";
-import ConfirmEmailModal from "./components/confirmEmailModal";
-import AddProfessionalPhoto from "./components/AddProfessionalPhoto";
+import ATS from "./components/ATS";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <AddProfessionalPhoto/>
+      <Header />
+      <ATS />
+      <Footer />
     </div>
   );
 }
