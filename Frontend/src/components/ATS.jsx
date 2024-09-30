32b492948f1c3bcb0ed5a4c264ac7c5a96882a9b
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2024-10-01 03:43:03 +0530

    Refactor ATS component to include Jobs section

--- Frontend/src/components/ATS.jsx
+++ Frontend/src/components/ATS.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Sidebar from './ATS/Sidebar';
 import Dashboard from './ATS/Dashboard';
+import Jobs from './ATS/Jobs';
 import './css/ATS.css';
 
 const ATS = () => {
   return (
     <div className="ATS-container">
       <Sidebar />
-      <Dashboard />
+      <Jobs />
     </div>
   );
 };
