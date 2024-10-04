d911542a1ce34d03bc668feb25dc5953d5e56426
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2024-10-05 02:26:25 +0530

    Refactor App component to use FeedSection instead of Home

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/includes/header";
 import Footer from "./components/includes/footer";
 import FeedSection from "./components/feedSection";
 import ATS from "./components/ATS";
-import Home from "./home.jsx";
+// import Home from "./home.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <FeedSection />
       <Footer />
     </div>
   );
