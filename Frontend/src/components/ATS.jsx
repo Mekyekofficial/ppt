07f4d52b1597921c71074cf796762ef9722e45ac
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2024-10-05 03:36:57 +0530

    Refactor ATS component to use Applications instead of Jobs

--- Frontend/src/components/ATS.jsx
+++ Frontend/src/components/ATS.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Sidebar from './ATS/Sidebar';
-import Dashboard from './ATS/Dashboard';
-import Jobs from './ATS/Jobs';
+// import Dashboard from './ATS/Dashboard';
+// import Jobs from './ATS/Jobs';
+import Applications from './ATS/Applications';
 import './css/ATS.css';
 
 const ATS = () => {
   return (
     <div className="ATS-container">
       <Sidebar />
-      <Jobs />
+      <Applications />
     </div>
   );
 };
