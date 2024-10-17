4566a394c9a3c7745f53a5f466ff8d4420301b3d
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2024-10-17 23:01:04 +0530

    Refactor ATS Sidebar component to use react-router-dom for navigation

--- Frontend/src/components/ATS/Sidebar.jsx
+++ Frontend/src/components/ATS/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from '../../assets/logo.png';
 import { FaHome, FaBriefcase, FaEnvelope, FaUsers, FaSearch, FaStore, FaCog, FaList } from 'react-icons/fa';
 import SidebarStyles from './css/sidebar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
@@ -14,29 +15,45 @@ const Sidebar = () => {
         <input type="text" placeholder="Search..." className={SidebarStyles["search-box"]} />
       </div>
       <ul className={SidebarStyles.menu}>
-        <li className={SidebarStyles.active}>
-          <FaHome className={SidebarStyles["menu-icon"]} /> Dashboard
+        <li>
+          <NavLink to="/dashboard" activeClassName={SidebarStyles["active"]} className={SidebarStyles["menu-item"]}>
+            <FaHome className={SidebarStyles["menu-icon"]} /> Dashboard
+          </NavLink>
         </li>
         <li>
-          <FaBriefcase className={SidebarStyles["menu-icon"]} /> Jobs
+          <NavLink to="/jobs" activeClassName={SidebarStyles["active"]} className={SidebarStyles["menu-item"]}>
+            <FaBriefcase className={SidebarStyles["menu-icon"]} /> Jobs
+          </NavLink>
         </li>
         <li>
-          <FaEnvelope className={SidebarStyles["menu-icon"]} /> Applications
+          <NavLink to="/applications" activeClassName={SidebarStyles["active"]} className={SidebarStyles["menu-item"]}>
+            <FaEnvelope className={SidebarStyles["menu-icon"]} /> Applications
+          </NavLink>
         </li>
         <li>
-          <FaUsers className={SidebarStyles["menu-icon"]} /> Talent Pool
+          <NavLink to="/talent-pool" activeClassName={SidebarStyles["active"]} className={SidebarStyles["menu-item"]}>
+            <FaUsers className={SidebarStyles["menu-icon"]} /> Talent Pool
+          </NavLink>
         </li>
         <li>
-          <FaList className={SidebarStyles["menu-icon"]} /> Talent Hunt
+          <NavLink to="/talent-hunt" activeClassName={SidebarStyles["active"]} className={SidebarStyles["menu-item"]}>
+            <FaList className={SidebarStyles["menu-icon"]} /> Talent Hunt
+          </NavLink>
         </li>
         <li>
-          <FaStore className={SidebarStyles["menu-icon"]} /> Market Place
+          <NavLink to="/market-place" activeClassName={SidebarStyles["active"]} className={SidebarStyles["menu-item"]}>
+            <FaStore className={SidebarStyles["menu-icon"]} /> Market Place
+          </NavLink>
         </li>
         <li>
-          <FaCog className={SidebarStyles["menu-icon"]} /> Settings
+          <NavLink to="/settings" activeClassName={SidebarStyles["active"]} className={SidebarStyles["menu-item"]}>
+            <FaCog className={SidebarStyles["menu-icon"]} /> Settings
+          </NavLink>
         </li>
         <li>
-          <FaList className={SidebarStyles["menu-icon"]} /> Setup
+          <NavLink to="/setup" activeClassName={SidebarStyles["active"]} className={SidebarStyles["menu-item"]}>
+            <FaList className={SidebarStyles["menu-icon"]} /> Setup
+          </NavLink>
         </li>
       </ul>
     </div>
