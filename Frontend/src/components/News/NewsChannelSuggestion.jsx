08a0f159bdd72a0d5d1b1d8707e97cc6882802ba
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2024-10-22 03:14:53 +0530

    Add NewsChannelSuggestion component with CSS styling

---
+++ Frontend/src/components/News/NewsChannelSuggestion.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './css/NewsChannelSuggestion.css';
+
+const NewsChannelSuggestion = () => {
+  return (
+    <div className="news-channel-suggestion">
+      <h3>You can check out...</h3>
+      <ul>
+        <li>User 1</li>
+        <li>User 2</li>
+        <li>User 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NewsChannelSuggestion;
