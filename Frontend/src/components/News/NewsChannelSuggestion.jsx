0adff8b51a75871c2a6c542d13f2b875a83eeeaa
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2024-10-23 03:46:09 +0530

    Refactor NewsChannelSuggestion component styles and update user list

--- Frontend/src/components/News/NewsChannelSuggestion.jsx
+++ Frontend/src/components/News/NewsChannelSuggestion.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import NewsChannelSuggestionStyles from './css/NewsChannelSuggestion.module.css';
+import React from "react";
+import { FaRegCircleUser } from "react-icons/fa6";
+import styles from "./css/NewsChannelSuggestion.module.css";
+
+const users = [
+  "User 1",
+  "User 2",
+  "User 3",
+  "User 4",
+  "User 5",
+];
 
 const NewsChannelSuggestion = () => {
   return (
-    <div className={NewsChannelSuggestionStyles["news-channel-suggestion"]}>
-      <h3>You can check out...</h3>
-      <ul>
-        <li>User 1</li>
-        <li>User 2</li>
-        <li>User 3</li>
-      </ul>
+    <div className={styles.container}>
+      <h2>You can check out...</h2>
+      <div className={styles.userList}>
+        {users.map((user, index) => (
+          <div key={index} className={styles.userItem}>
+            <div className={styles.userInfo}>
+              <span className={styles.userIcon}><FaRegCircleUser /></span>
+              <span className={styles.userName}>{user}</span>
+            </div>
+            <div className={styles.actions}>
+              <button className={styles.actionButton}>Go after</button>
+              <button className={styles.actionButton}>Be in touch</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
