54fa417c38f01c1abed2565451a970ee5146893a
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2024-10-23 03:45:51 +0530

    Refactor NewsRecommendation component styles and update recommended topics

--- Frontend/src/components/News/NewsRecommendation.jsx
+++ Frontend/src/components/News/NewsRecommendation.jsx
@@ -6,11 +6,11 @@ const NewsRecommendation = () => {
     <div className={NewsRecommendationStyles["news-recommendation"]}>
       <h3>Recommended Topics</h3>
       <div className={NewsRecommendationStyles.topics}>
-        <span>Technology</span>
-        <span>Web</span>
-        <span>Travel</span>
-        <span>Finance</span>
-        <span>India</span>
+        <span>+ Technology</span>
+        <span>+ Web</span>
+        <span>+ Travel</span>
+        <span>+ Finance</span>
+        <span>+ India</span>
       </div>
     </div>
   );
