aa93ac57e001c23204320a30ec1d15523bf33c9b
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2024-12-13 19:19:58 +0530

    Redirect /ATS to /ATS/Dashboard for improved navigation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 
 import Home from "./home";
 import AppOutlet from "./AppOutlet";
@@ -83,7 +83,7 @@ const App = () => {
           children: [
             {
               path: "/ATS",
-              element: <Dashboard />,
+              element: <Navigate to="/ATS/Dashboard" />,
             },
             {
               path: "/ATS/Dashboard",
