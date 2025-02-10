72bd4443be2c283f7ff96578b3052d99fa0e0e2d
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2025-02-10 18:16:51 +0530

    Refactor header component; update styles for improved layout and add sign-up button functionality

--- FrontEnd/src/components/includes/headerBeforeLogIn.jsx
+++ FrontEnd/src/components/includes/headerBeforeLogIn.jsx
@@ -1,44 +1,22 @@
-import React from 'react';
-import Logo from '../../assets/logo.png';
-import HeaderStyles from './css/headerBeforeLogIn.module.css';
-import { Navigate, NavLink } from 'react-router-dom';
-import { useState } from 'react';
-import LoginSignupPop from '../LoginSignupPop';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./css/headerBeforeLogIn.module.css";
 
 const HeaderBeforeLogIn = () => {
-  const [isLoginPopupOpen, setIsLoginPopupOpen] = useState(false);
-
-  const onLogInClick = () => {
-    if (!isLoginPopupOpen) {
-      setIsLoginPopupOpen(true);
-    } else {
-      setIsLoginPopupOpen(false);
-    }
-  };
-
   return (
-    <header className={HeaderStyles.header}>
-      {/* Logo */}
-      <div className={HeaderStyles.logo} onClick={() => Navigate('/')}>
-        <img src={Logo} alt="Logo" className={HeaderStyles["logo-image"]} />
-        <span className={HeaderStyles["logo-text"]}>Mekyek</span>
-      </div>
-
+    <header className={styles.header}>
       {/* Navigation Links */}
-      <nav className={HeaderStyles.nav}>
-        <NavLink to="/feeds" className={HeaderStyles["nav-item"]}>
-          <span>Hire Talent</span>
+      <nav className={styles.nav}>
+        <NavLink to="/hire-talent" className={styles.navItem}>
+          Hire Talent
         </NavLink>
-        <NavLink to="/feeds" className={HeaderStyles["nav-item"]}>
-          <span>Find Work</span>
+        <NavLink to="/find-work" className={styles.navItem}>
+          Find Work
         </NavLink>
       </nav>
 
-      {/* User Section */}
-      <div className={HeaderStyles["user-section"]} onClick={onLogInClick}>
-        Log In
-      </div>
-      {isLoginPopupOpen && <LoginSignupPop onLogInClick={onLogInClick} />}
+      {/* Sign Up Button */}
+      <button className={styles.signUpButton}>Sign Up</button>
     </header>
   );
 };
