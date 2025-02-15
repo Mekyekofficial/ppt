1e877a23113eb7ac3ed7f1b0698337fb65222f92
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2025-02-16 01:24:51 +0530

    Add ProfileEducation component and enhance ProfileSkills layout; update styles for better UX

--- FrontEnd/src/components/Profile/ProfileSkills.jsx
+++ FrontEnd/src/components/Profile/ProfileSkills.jsx
@@ -6,15 +6,14 @@ import { FiEdit2 } from "react-icons/fi";
 const ProfileSkills = () => {
   return (
     <div className={styles.container}>
-      {/* Edit Button at the top right */}
-      <div className={styles.editIcon}>
-        <FiEdit2 />
-      </div>
-
-      {/* Header with Skills Title and Star Icon */}
       <div className={styles.header}>
         <div className={styles.title}>
           Skills <FaStar className={styles.icon} />
+        </div>
+        <div className={styles.editIcon}>
+          <svg className={styles.editIcon} viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M41.6667 158.333H53.5417L135 76.875L123.125 65L41.6667 146.458V158.333ZM25 175V139.583L146.875 18.125L181.667 53.75L60.4167 175H25ZM128.958 71.0417L123.125 65L135 76.875L128.958 71.0417Z" fill="black"/>
+                  </svg>
         </div>
       </div>
 
@@ -38,8 +37,11 @@ const ProfileSkills = () => {
       </div>
 
       {/* Footer */}
-      <div className={styles.footer}>
-        <span className={styles.showMore}>Show More ▼</span>
+      <div className={styles.showMore}>
+              Show More 
+              <svg viewBox="0 0 227 227" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M66.2109 94.5835L113.503 141.875L160.794 94.5835" stroke="black" stroke-width="22.67" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
       </div>
     </div>
   );
