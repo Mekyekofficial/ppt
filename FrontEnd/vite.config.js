c42f7172b0ac39899a8d8dc9382fec81bae97383
Suddhajit2003 <suddhajit2@gmail.com>
2025-02-21 03:30:56 +0530

    changed port

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -4,6 +4,12 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // Ensure this line is present
