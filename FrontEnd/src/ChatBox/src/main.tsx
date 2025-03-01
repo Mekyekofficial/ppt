640620e64baf0b2d6407234cd8010d5b867e0198
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2025-03-02 00:09:45 +0530

    Remove ChatBox component and associated files

--- FrontEnd/src/ChatBox/src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client'
-// import Chat from './chat.tsx'
-import Index from './index';
-import './index.css'
-
-createRoot(document.getElementById("root")!).render(<Index />);
