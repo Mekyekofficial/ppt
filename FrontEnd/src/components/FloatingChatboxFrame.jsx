cf5741785650fa8feac2e2dff688b580676f4609
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2025-03-16 17:49:53 +0530

    Enhance FloatingChatboxFrame component: improve mouse interaction for minimizing, adjust styles, and add styled-components dependency.

--- FrontEnd/src/components/FloatingChatboxFrame.jsx
+++ FrontEnd/src/components/FloatingChatboxFrame.jsx
@@ -4,25 +4,28 @@ const FloatingChatboxFrame = () => {
   const [isMinimized, setIsMinimized] = useState(true);
 
   return (
-    <div style={{
-      position: 'fixed',
-      bottom: '0',
-      right: '0',
-      zIndex: 1000000,
-      cursor: 'pointer !important',
-      backgroundColor: 'white',
-    }}>
+    <div 
+      style={{
+        position: 'fixed',
+        bottom: 0,
+        right: 0,
+        zIndex: 1000000,
+        width: isMinimized ? '10vw' : '100vw',
+        height: isMinimized ? '10vh' : '100vh',
+      }}
+      onMouseEnter={() => setIsMinimized(false)}
+      onMouseLeave={() => setIsMinimized(true)}
+    >
       <iframe
-          src="http://localhost:8000"
-          style={{
-            border: 'none',
-            boxShadow: 'none',
-            width: isMinimized ? '10vw' : '100vw',
-            height: isMinimized ? '10vh' : '100vh',
-          }}
-        />
+        src="http://localhost:8000"
+        style={{
+          width: '100%',
+          height: '100%',
+          border: 'none',
+        }}
+      />
     </div>
   );
 };
 
-export default FloatingChatboxFrame; 
+export default FloatingChatboxFrame;
