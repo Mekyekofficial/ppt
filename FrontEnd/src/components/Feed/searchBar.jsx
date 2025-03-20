ecadfa170a89a26a6b3e7dd0c18100768848b78b
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2025-03-21 00:57:00 +0530

    Fix import case sensitivity for SearchBar styles

--- FrontEnd/src/components/Feed/searchBar.jsx
+++ FrontEnd/src/components/Feed/searchBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import {useNavigate} from "react-router-dom";
-import styles from "./css/SearchBar.module.css";
+import styles from "./css/searchBar.module.css";
 import ProfileImage from "../../assets/profile-image.png";
 import { toast } from "react-toastify";
 import API from "../../api";
