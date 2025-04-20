9474b589273a40848c15b823d3606819ce499e67
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2025-04-21 03:09:19 +0530

    Update API base URL to localhost and adjust start script for development

--- FrontEnd/src/api.js
+++ FrontEnd/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://server.mekyek.com/', // Adjust the URL based on your backend
+  baseURL: 'http://localhost:5000/', // Adjust the URL based on your backend
 });
 
 // Add login request
